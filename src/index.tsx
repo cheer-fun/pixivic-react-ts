dd029a82bc42f8360018bf37b00f6313691ac431
kimliy <1924429596@qq.com>
2019-11-08 22:42:59 +0800

    build: init

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'amfe-flexible/index.js';
+import './index.styl';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
